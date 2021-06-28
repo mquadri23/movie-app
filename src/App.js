2eafceca57a80a617096543d9b36d9139fe22086
Maria Quadriq <mquadri23@gmail.com>
2021-06-27 22:13:35 -0500

    completed app with original API

--- src/App.js
+++ src/App.js
@@ -8,20 +8,54 @@ const SEARCH_API =
 
 function App() {
   const [movies , setMovies] = useState([])
+  const [searchTerm,setSearchTerm] = useState('')
+
   useEffect(() => {
-    fetch (API_URL)
+   getMovies(API_URL)
+  },[])
+
+  function getMovies (API){
+    fetch (API)
     .then(res => res.json())
     .then(data => {
       console.log(data)
       setMovies(data.results)
     })
-  },[])
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+
+    if (searchTerm){
+      getMovies(SEARCH_API + searchTerm)
+      setSearchTerm ('')
+    }
+   
+  }
+
+  const handleOnChange = (e) =>{
+    setSearchTerm(e.target.value)
+  }
 
   return (
     <div>
+    <header>
+      <form onSubmit={handleOnSubmit}>
+        <input 
+          className='search' 
+          type='search' 
+          placeholder='Search...' 
+          value={searchTerm}
+          onChange={handleOnChange}/>
+      </form>
+      
+      </header>
+
+    <div className='movie-container'>
        {movies.length > 0 && movies.map((movie) => (
          <Movie key={movie.id} {...movie}/>
        ))}
+    </div>
     </div>
   );
 }
